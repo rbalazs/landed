35d241410fa7c6d407f0abd17a707f5895e354de
Richer Balázs <richer.balazs@gmail.com>
2019-09-21 10:32:10 +0200

    Symfony 4.3 Process Component setup.

--- src/Controller/OverviewController.php
+++ src/Controller/OverviewController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 
 class OverviewController extends AbstractController
 {
@@ -12,6 +14,15 @@ class OverviewController extends AbstractController
      */
     public function index()
     {
+        $process = new Process(['git', 'status'], '/app/repos');
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        echo $process->getOutput();
+
         return $this->render('overview/index.html.twig', [
             'controller_name' => 'OverviewController',
         ]);
