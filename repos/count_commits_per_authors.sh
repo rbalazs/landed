b79e7bd29223662077493c2618a20d99affb65b9
Richer Balázs <richer.balazs@gmail.com>
2019-10-08 13:39:14 +0200

    Exclude merge commits from statistics.

--- repos/count_commits_per_authors.sh
+++ repos/count_commits_per_authors.sh
@@ -4,5 +4,5 @@ dir=$1
 
 cd "$dir"
 
-git log | grep Author |  awk '{print $4}' | sed "s/<//g" | sed "s/>//g" | sort | uniq -c | sort -rg
+git log --no-merges | grep Author |  awk '{print $4}' | sed "s/<//g" | sed "s/>//g" | sed "s/@[a-z]*\.[a-z]*//g" | sort | uniq -c | sort -rg
 
--- repos/count_commits_per_weekday.sh
+++ repos/count_commits_per_weekday.sh
@@ -4,5 +4,5 @@ dir=$1
 
 cd "$dir"
 
-git log | grep Date |  awk '{print $2}' | sort | uniq -c | sort -rg
+git log --no-merges | grep Date |  awk '{print $2}' | sort | uniq -c | sort -rg
 
