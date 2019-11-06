3523fdf9e9b0464ab1216d7ebb222f32a0473ac3
Richer Balázs <richer.balazs@gmail.com>
2019-11-06 06:49:46 +0100

    Line chart for commit per hour.

--- repos/count_commits_per_hour.sh
+++ repos/count_commits_per_hour.sh
@@ -4,5 +4,5 @@ dir=$1
 
 cd "$dir"
 
-git log | grep Date |  awk '{print $5}' | awk -F ":" '{print $1}' | sort | uniq -c | sort -rg
+git log | grep Date |  awk '{print $5}' | awk -F ":" '{print $1}' | sort | uniq -c
 
